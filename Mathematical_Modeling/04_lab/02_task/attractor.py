b491ab18d7c260316187a5c8399e77bedc748c0b
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-10-06 16:48:46 +0200

    task 2 improvements

--- Mathematical_Modeling/04_lab/02_task/attractor.py
+++ Mathematical_Modeling/04_lab/02_task/attractor.py
@@ -1,5 +1,5 @@
 from random import uniform
-from math import log
+from math import log, floor
 from argparse import ArgumentParser
 import png
 
@@ -29,32 +29,51 @@ def attractor(matrixes, vectors, x_target, y_target, iter_count):
     return points
 
 
-def create_png(points, filename, precision):
-    mult = 10**precision  # точность, сколько знаков после запятой рассматривать
-
+def create_png(points, filename):
     x_max = max(points, key=lambda x: x[0])[0]
     x_min = min(points, key=lambda x: x[0])[0]
 
     y_max = max(points, key=lambda y: y[1])[1]
     y_min = min(points, key=lambda y: y[1])[1]
 
-    dim_x = (x_max - x_min)*mult
-    dim_y = (y_max - y_min)*mult
-    
-    dim = int(max(dim_x, dim_y))+1
+    dim_x = x_max - x_min
+    dim_y = y_max - y_min
+
+    # Т.к. координаты могут быть не целыми, а пиксели всегда должны быть целыми, то нужно правильно преобразовать
+    # К примеру, точка (0.5, 0.8). В её случае лучше будет домножить на 10, тогда получим пискель (5, 8)
+    # Аналогично, точка (0.02, 0.06), тут домножаем на 100 и имеем (2, 6) и т.д.
+    # А вот если точка (15, 29), то тогда лучше ничего не менять, ей подойдёт пиксель (15, 29)
+
+    power = log(min(dim_x, dim_y))/log(10)
+    mult = 1 
+
+    if 0 <= power < 1:
+        mult = 10
+
+    elif power < 0:
+        mult = 10**abs(floor(power))
+
+    dim = int(max(dim_x, dim_y)*mult)+1
 
     image_data = [[1 for i in range(dim)] for j in range(dim)]
 
+    pixel_count = 0
+
     for point in points:
-        x = int((point[0] - x_min)*mult)
+        x = int((point[0] - x_min)*mult)  # сдвиг нужен, чтобы не было отрицательных координат
         y = int((point[1] - y_min)*mult)
+
+        if image_data[y][x] == 1:
+            pixel_count += 1
 
         image_data[y][x] = 0
 
     image = png.from_array(image_data, "L;1")  # L;1 - greyscale with bitdepth 1
     image.save(filename)
 
-    return dim
+    size = log(pixel_count) / log(dim)
+
+    return size
 
 
 def parse():
@@ -62,7 +81,6 @@ def parse():
 
     parser.add_argument("-x", required=True, type=int, help="the x coordinate for the root of fractal")
     parser.add_argument("-y", required=True, type=int, help="the y coordinate for the root of fractal")
-    parser.add_argument("-p", required=True, type=int, help="the rounding of floating point")
     parser.add_argument("-i", required=True, type=int, help="the number of iterations")
 
     return parser.parse_args()
@@ -92,9 +110,9 @@ def main():
 
     points = attractor(matrixes, vectors, args.x, args.y, args.i)
 
-    dim = create_png(points, f"{args.i}_iterations.png", args.p)
+    size = create_png(points, f"{args.i}_iterations.png")
 
-    print("Fractal size =", log(args.i) / log(dim))
+    print("Fractal size =", size)
 
 
 if __name__ == "__main__":
