101548679e4cc051112e5c9aed7e8de2e6cf1d8e
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-10-27 18:36:40 +0200

    lab 6 started

---
+++ Mathematical_Modeling/06_lab/diffusion.py
@@ -0,0 +1,129 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+
+
+def Ut0(x):
+    return x*(10-x)**2
+
+
+def Ux0(t):
+    return 0
+
+
+def Ux1(t):
+    return 0
+
+
+def D(x):
+    return x+1
+
+
+def f(x):
+    return 11-x
+
+
+def explicit_method(a, b, c, d, h, T):
+    x = np.arange(a, b + h, h)
+    t = np.arange(c, d + T, T)
+    height, width = len(t), len(x)
+
+    U = np.zeros((height, width))
+
+    for j in range(height):
+        for i in range(width):
+            if i == 0:
+                U[j][i] = Ux0(t[j])
+
+            elif i == width - 1:
+                U[j][i] = Ux1(t[j])
+
+            elif j == 0:
+                U[j][i] = Ut0(x[i])
+
+            else:
+                l = D(x[i])*T/(h**2)
+                U[j][i] = (l*U[j-1][i+1] + (1-2*l)*U[j-1][i] + l*U[j-1][i-1] + 5) / (1+T*f(x[i]))
+
+    return [x, t, U]
+
+
+def implicit_method(a, b, c, d, h, T):
+    x = np.arange(a, b + h, h)
+    t = np.arange(c, d + T, T)
+    height, width = len(t), len(x)
+
+    U = np.zeros((height, width))
+
+    for j in range(height):  # граничные условия
+        U[j][0] = Ux0(t[j])
+        U[j][width-1] = Ux1(t[j])
+
+    for i in range(1, width-1):  # начальные условия
+        U[0][i] = Ut0(x[i])
+
+    # Далее метод прогонки
+
+    for j in range(1, height):
+        # Сначала находим прогоночные коэф. альфа и бета
+
+        alpha = np.zeros(width-1)
+        beta = np.zeros(width-1)
+        beta[0] = Ux0(t[j])  # ВАЖНО! Иначе всё неправильно
+
+        for i in range(1, width-1):
+            l = D(x[i])*T/(h**2)
+
+            A = l
+            B = -1 - 2*l - f(x[i])
+            C = l
+            F = -U[j-1][i] - 5*T
+
+            alpha[i] = -A / (C*alpha[i-1] + B)
+            beta[i] = (F - C*beta[i-1]) / (C*alpha[i-1] + B)
+
+        # Потом, начиная с конца, находим все значения U[j][i]
+
+        for i in range(width-2, 0, -1):
+            U[j][i] = alpha[i]*U[j][i+1] + beta[i]
+
+    return [x, t, U]
+
+   
+def plot3d(points):
+    ax = plt.figure().add_subplot(projection="3d")
+    X, T = np.meshgrid(points[0], points[1])
+    
+    ax.plot_surface(X, T, points[2], cmap=cm.magma)
+    ax.set_xlabel("x")
+    ax.set_ylabel("t")
+    ax.set_zlabel("U(x, t)")
+
+    plt.show()
+
+
+def main():
+    x_min, x_max = 0, 10
+    t_min, t_max = 0, 10
+
+    print("Количество разбиений по x: ")
+    width = int(input())
+
+    h = (x_max - x_min)/width
+    T = h**2 / (2*D(x_max))  # из условия устойчивости
+
+    print("Решение каким методом вывести? (1-2)\n1). Явный метод\n2). Неявный метод\n")
+    select = input()
+
+    if select == "1":
+        plot3d(explicit_method(x_min, x_max, t_min, t_max, h, T))
+
+    elif select == "2":
+        plot3d(implicit_method(x_min, x_max, t_min, t_max, h, T))
+
+    else:
+        print("Такого шаблона нет!")
+
+
+if __name__ == "__main__":
+    main()
