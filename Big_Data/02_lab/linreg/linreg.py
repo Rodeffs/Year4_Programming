ea1cf4930c1f546779bfc8245bd7f7469b98ce02
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-12-01 22:41:46 +0200

    linreg done

---
+++ Big_Data/02_lab/linreg/linreg.py
@@ -0,0 +1,45 @@
+def reader(filepath):
+    with open(filepath, mode="r", encoding="utf-8") as f:
+        while True:
+            line = f.readline()
+
+            if not line:
+                break
+
+            yield line
+
+
+def mapper(filepath):
+    for line in reader(filepath):
+        x, y = line.split()
+        yield float(x), float(y)
+
+
+def reducer(points):
+    N = 0
+    sumX, sumY, sumXY, sumX2 = 0, 0, 0, 0
+
+    for point in points:
+        x, y = point
+        sumX += x
+        sumY += y
+        sumXY += x*y
+        sumX2 += x**2
+        N += 1
+
+    b1 = (N*sumXY - sumX*sumY)/(N*sumX2 - sumX**2)
+    b0 = (sumY - b1*sumX)/N
+
+    return b1, b0
+
+
+def main():
+    filepath = "xy.txt"
+
+    b1, b0 = reducer(mapper(filepath))
+
+    print(f"Y = {b1}*X + {b0}")
+
+
+if __name__ == "__main__":
+    main()
