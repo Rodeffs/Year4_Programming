f8bf328725634b00af516d8f3387644337c01f13
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-12-07 00:50:08 +0200

    concatanated all datasets into one singular to get better result

--- Big_Data/03_lab/airflow_dags/hadoop_airflow.py
+++ Big_Data/03_lab/airflow_dags/hadoop_airflow.py
@@ -6,14 +6,14 @@ from datetime import datetime
 HADOOP_HOME="/home/owner/Downloads/Big_Data/hadoop"
 HADOOP_STREAMING=f"{HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
 SOURCE_DIR="/home/owner/Programming/Year4_Programming/Big_Data/03_lab/hadoop"
-INPUT_DIR="/home/owner/Downloads/Big_Data/split"
+INPUT_FILE="/home/owner/Downloads/Big_Data/cleaned_papers.txt"
 OUTPUT_DIR="/home/owner/Downloads/Big_Data/hadoop_output"
 RESULT_FILE="/home/owner/Downloads/Big_Data/hadoop_result.txt"
 
 os.environ["JAVA_HOME"]="/usr/lib/jvm/java-17-openjdk"
 
 with DAG(dag_id="popular-topics-hadoop", start_date=datetime(2025, 12, 6), schedule="@weekly", catchup=False) as dag:
-    hadoop_mapreduce = BashOperator(task_id="hadoop-mapreduce", bash_command=f"{HADOOP_HOME}/bin/hadoop jar {HADOOP_STREAMING} -input {INPUT_DIR} -output {OUTPUT_DIR} -mapper 'python {SOURCE_DIR}/mapper.py' -reducer 'python {SOURCE_DIR}/reducer.py'")
+    hadoop_mapreduce = BashOperator(task_id="hadoop-mapreduce", bash_command=f"{HADOOP_HOME}/bin/hadoop jar {HADOOP_STREAMING} -input {INPUT_FILE} -output {OUTPUT_DIR} -mapper 'python {SOURCE_DIR}/mapper.py' -reducer 'python {SOURCE_DIR}/reducer.py'")
 
     sort_results = BashOperator(task_id="sorting-results", bash_command=f"cat {OUTPUT_DIR}/part-* | sort -t';' -k2,2nr > {RESULT_FILE}")
 
