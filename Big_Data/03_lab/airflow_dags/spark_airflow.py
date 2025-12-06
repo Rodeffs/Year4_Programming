44e7569c3f5b527fbb4693ac5e28c740bc50aa81
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-12-06 23:23:13 +0200

    spark optimisations

--- Big_Data/03_lab/airflow_dags/spark_airflow.py
+++ Big_Data/03_lab/airflow_dags/spark_airflow.py
@@ -9,7 +9,7 @@ SOURCE_FILE="/home/owner/Programming/Year4_Programming/Big_Data/03_lab/spark/spa
 os.environ["JAVA_HOME"]="/usr/lib/jvm/java-17-openjdk"
 
 with DAG(dag_id="popular-topics-spark", start_date=datetime(2025, 12, 6), schedule="@weekly", catchup=False) as dag:
-    spark_job = BashOperator(task_id="spark-job", bash_command=f"{SPARK_HOME}/bin/spark-submit --num-executors 4 --executor-memory 4G --executor-cores 4 {SOURCE_FILE}")
+    spark_job = BashOperator(task_id="spark-job", bash_command=f"{SPARK_HOME}/bin/spark-submit --num-executors 3 --executor-memory 6G --executor-cores 2 --driver-memory 4G --conf spark.memory.fraction=0.6 --conf spark.memory.storeageFraction=0.3 {SOURCE_FILE}")
 
     spark_job
 
