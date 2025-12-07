4c15facf7fb6483ae5f0e528f0705857e3643744
Rodeffs <guideandplaygamespilex24@gmail.com>
2025-12-07 02:23:10 +0200

    now actually utilising hdfs

--- Big_Data/03_lab/airflow_dags/hadoop_airflow.py
+++ Big_Data/03_lab/airflow_dags/hadoop_airflow.py
@@ -7,17 +7,20 @@ HADOOP_HOME="/home/owner/Downloads/Big_Data/hadoop"
 HADOOP_STREAMING=f"{HADOOP_HOME}/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
 SOURCE_DIR="/home/owner/Programming/Year4_Programming/Big_Data/03_lab/hadoop"
 INPUT_FILE="/home/owner/Downloads/Big_Data/cleaned_papers.txt"
+INPUT_DIR="/home/owner/Downloads/Big_Data/hadoop_input"
 OUTPUT_DIR="/home/owner/Downloads/Big_Data/hadoop_output"
 RESULT_FILE="/home/owner/Downloads/Big_Data/hadoop_result.txt"
 
 os.environ["JAVA_HOME"]="/usr/lib/jvm/java-17-openjdk"
 
 with DAG(dag_id="popular-topics-hadoop", start_date=datetime(2025, 12, 6), schedule="@weekly", catchup=False) as dag:
-    hadoop_mapreduce = BashOperator(task_id="hadoop-mapreduce", bash_command=f"{HADOOP_HOME}/bin/hadoop jar {HADOOP_STREAMING} -input {INPUT_FILE} -output {OUTPUT_DIR} -mapper 'python {SOURCE_DIR}/mapper.py' -reducer 'python {SOURCE_DIR}/reducer.py'")
+    hadoop_hdfs = BashOperator(task_id="hadoop-hdfs-init", bash_command=f"{HADOOP_HOME}/bin/hdfs dfs -mkdir {INPUT_DIR}; {HADOOP_HOME}/bin/hdfs dfs -put {INPUT_FILE} {INPUT_DIR}")
 
-    sort_results = BashOperator(task_id="sorting-results", bash_command=f"cat {OUTPUT_DIR}/part-* | sort -t';' -k2,2nr > {RESULT_FILE}")
+    hadoop_mapreduce = BashOperator(task_id="hadoop-mapreduce", bash_command=f"{HADOOP_HOME}/bin/hadoop jar {HADOOP_STREAMING} -input {INPUT_DIR} -output {OUTPUT_DIR} -mapper 'python {SOURCE_DIR}/mapper.py' -reducer 'python {SOURCE_DIR}/reducer.py'")
 
-    cleanup = BashOperator(task_id="cleaning-up", bash_command=f"rm -rf {OUTPUT_DIR}")
+    sort_results = BashOperator(task_id="sorting-results", bash_command=f"{HADOOP_HOME}/bin/hdfs dfs -cat {OUTPUT_DIR}/part-* | sort -t';' -k2,2nr > {RESULT_FILE}")
 
-    hadoop_mapreduce >> sort_results >> cleanup
+    cleanup = BashOperator(task_id="cleaning-up", bash_command=f"rm -rf {OUTPUT_DIR} {INPUT_DIR}")
+
+    hadoop_hdfs >> hadoop_mapreduce >> sort_results >> cleanup
 
